64488531d533deeddb4f326454ebde2cc1f75e36
IvanSlivenko <ivan8822@ulr.net>
2023-09-08 22:52:55 +0300

    08-09-2023-22-52

--- PractickReact/React-projectThrее/budget/src/componentns/Home/index.spec.js
+++ PractickReact/React-projectThrее/budget/src/componentns/Home/index.spec.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { mount } from "enzyme";
+
+import { act } from 'react-dom/test-utils'
 import Home from ".";
+// import jest from "jest";
 
 import { getItems, addItem } from "../../utils/indexdb";
 
@@ -15,28 +18,71 @@ describe('Home component', () => {
     let props;
 
 
-    beforeEach(() => {
-        sut = mount(<Home {...props} />);
-    });
-
-
-
     describe('when component is mounted', () => {
 
-        it('should set balance to 0', () => {
-            const { balance } = sut.find("Balance").at(0).props();
-
-            expect(balance).toBe(0);
-
-        });
+        
         describe('when transactions are return  successfully ', () => {
             deforeEach(() => {
-                getItems.mockResolvedValue([{
-                    value:1}])
+
+                getItems.mockImplementation(() => Promise.resolve([{ value: 1 }]))
             });
 
-        it('should render transactions with one item',()=>{})
+            beforeEach(async () => {
+                
+                await act(async () => {
+                    sut = mount(<Home {...props} />);
+                });
+                sut.update();
+            });
 
-        })
+            it('should set balance to 0', () => {
+                const { balance } = sut.find("Balance").at(0).props();
+
+                expect(balance).toBe(0);
+
+            });
+
+
+            it('should render transactions with one item', () => { })
+
+            
+            const { transactions } = sut.find('Transactions').at(0).props();
+
+            // console.log('---', transactions);
+            
+            expect(transactions).toEgual([{ value: 1 }])
+        });
+
+        describe('when transactions are return  with error ', () => {
+            deforeEach(() => {
+
+                getItems.mockImplementation(() => Promise.reject('My Error'))
+            });
+
+            beforeEach(async () => {
+                
+                await act(async () => {
+                    sut = mount(<Home {...props} />);
+                });
+                sut.update();
+            });
+
+            
+
+
+            it('should render transactions with zero item', () => { })
+
+            
+            const { transactions } = sut.find('Transactions').at(0).props();
+
+            // console.log('---', transactions);
+            
+            expect(transactions).toEgual([])
+        });
+
+        it('should console error rejected message', (() => {
+            
+        }))
+
     })
 })
